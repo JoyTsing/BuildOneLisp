7df79be6c53f52d25efcbccc78217cf9c9a1c8e1
joyTsing <792566033@qq.com>
2020-11-28 19:08:09 +0800

    增添初步的错误处理

--- clisp.c
+++ clisp.c
@@ -22,10 +22,100 @@ void add_history(char* unused){}
 #include<editline/history.h>
 #endif
 
-int number_of_nodes(mpc_ast_t* t);
-void printAstNode(mpc_ast_t* a);
-double eval(mpc_ast_t* t);
-double eval_op(double x,char* op,double y);
+//值类型
+enum{
+    LVAL_NUM,
+    LVAL_ERR
+};
+//错误类型
+enum{
+    LERR_DIV_ZERO,
+    LERR_BAD_OP,
+    LERR_BAD_NUM
+};
+
+typedef struct lval {
+    int type;//0:num,1:err
+    double num;
+    int err;
+}lval;
+
+//构造
+lval lval_num(double x){
+    lval v;
+    v.type=LVAL_NUM;
+    v.num=x;
+    return v;
+}
+
+//构造
+lval lval_err(int x){
+    lval v;
+    v.type=LVAL_ERR;
+    v.err=x;
+    return v;
+}
+
+void lval_print(lval v){
+    switch(v.type){
+        case LVAL_NUM:printf("%f",v.num);break;
+        case LVAL_ERR:
+            if(v.err==LERR_BAD_OP){
+                printf("Error: Invalid Operator");
+            }
+            if(v.err==LERR_BAD_NUM){
+                printf("Error: Invalid Number");
+            }
+            if(v.err==LERR_DIV_ZERO){
+                printf("Error: Division By Zero");
+            }
+        break;
+    }
+}
+
+void lval_println(lval v){
+    lval_print(v);
+    putchar('\n');
+}
+
+lval eval_op(lval x,char* op,lval y){
+    if(strcmp(op,"+")==0){return lval_num(x.num+y.num);}
+    if(strcmp(op,"-")==0){return lval_num(x.num-y.num);}
+    if(strcmp(op,"*")==0){return lval_num(x.num*y.num);}
+    if(strcmp(op,"/")==0){
+        return y.num==0?lval_err(LERR_DIV_ZERO):lval_num(x.num/y.num);
+    }
+    return lval_err(LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t* t){
+    if(strstr(t->tag,"number")){
+        errno=0;
+        double x=strtod(t->contents,NULL);
+        return errno!=ERANGE ? lval_num(x):lval_err(LERR_BAD_NUM);
+    }
+
+    //表达式
+    char* op=t->children[1]->contents;
+    lval ret=eval(t->children[2]);
+    int i=3;
+    while(strstr(t->children[i]->tag,"expr")){
+        ret=eval_op(ret,op,eval(t->children[i]));
+        i++;
+    }
+    return ret;
+}
+
+
+int number_of_nodes(mpc_ast_t* t){
+    if(t->children_num == 0)
+        return 1;
+    int total=1;
+    for(int i=0;i<t->children_num;i++){
+        total+=number_of_nodes(t->children[i]);
+    }
+    return total;
+}
 
 int main(int argc,char* argv[])
 {
@@ -58,8 +148,8 @@ int main(int argc,char* argv[])
         mpc_result_t r;
         if(mpc_parse("<stdin>",input,Lispy,&r)){
             
-            double result=eval(r.output);
-            printf("%f\n",result);
+            lval result=eval(r.output);
+            lval_println(result);
             //mpc_ast_print(r.output);
             mpc_ast_delete(r.output);
         }else{
@@ -73,37 +163,3 @@ int main(int argc,char* argv[])
     return 0;
 }
 
-double eval(mpc_ast_t* t){
-    if(strstr(t->tag,"number")){
-        return atof(t->contents);
-    }
-
-    //表达式
-    char* op=t->children[1]->contents;
-    double ret=eval(t->children[2]);
-    int i=3;
-    while(strstr(t->children[i]->tag,"expr")){
-        ret=eval_op(ret,op,eval(t->children[i]));
-        i++;
-    }
-    return ret;
-}
-
-
-double eval_op(double x,char* op,double y){
-    if(strcmp(op,"+")==0){return x+y;}
-    if(strcmp(op,"-")==0){return x-y;}
-    if(strcmp(op,"*")==0){return x*y;}
-    if(strcmp(op,"/")==0){return x/y;}
-    return 0;
-}
-
-int number_of_nodes(mpc_ast_t* t){
-    if(t->children_num == 0)
-        return 1;
-    int total=1;
-    for(int i=0;i<t->children_num;i++){
-        total+=number_of_nodes(t->children[i]);
-    }
-    return total;
-}
