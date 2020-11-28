8b2ded959d48fa971e1d9835d5a9127068b5f96d
joyTsing <792566033@qq.com>
2020-11-28 11:35:57 +0800

    add mpc

---
+++ practice/doge_grammer.c
@@ -0,0 +1,23 @@
+#include "../mpc.h"
+
+int main(int argc,char* argv[])
+{
+    mpc_parser_t* Adjective=mpc_new("adjective");
+    mpc_parser_t* Noun=mpc_new("noun");
+    mpc_parser_t* Phrase=mpc_new("phrase");
+    mpc_parser_t* Doge=mpc_new("doge");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+              "                                     \
+                adjective:\"wow\"|\"many\"          \
+                         | \"so\"|\"such\";         \
+                noun     :\"lisp\"|\"language\"     \
+                         |\"book\"|\"build\"|\"c\"; \
+                pharse   : <adjective> <noun>;      \
+                doge     : <phare>*;                \
+              ",
+              Adjective,Noun,Phrase,Doge);
+    mpc_cleanup(4,Adjective,Noun,Phrase,Doge);
+    return 0;
+}
+
