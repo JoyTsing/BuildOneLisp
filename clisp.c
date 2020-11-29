b412d29a05577e8053b4c2d724f655266adca9dd
joyTsing <792566033@qq.com>
2020-11-29 09:59:50 +0800

    S表达式转换与重构

--- clisp.c
+++ clisp.c
@@ -22,6 +22,7 @@ void add_history(char* unused){}
 #include<editline/history.h>
 #endif
 
+
 //值类型
 enum{
     LVAL_NUM,
@@ -47,6 +48,9 @@ typedef struct lval {
     int count;
     struct lval** cell;//not only one
 }lval;
+
+void lval_expr_print(lval*,char,char);
+void lval_print(lval*);
 
 //construct
 lval* lval_num(double x){
@@ -96,28 +100,73 @@ void lval_del(lval* v){
     free(v);
 }
 
-void lval_print(lval v){
-    switch(v.type){
-        case LVAL_NUM:printf("%f",v.num);break;
-        case LVAL_ERR:
-            if(v.err==LERR_BAD_OP){
-                printf("Error: Invalid Operator");
-            }
-            if(v.err==LERR_BAD_NUM){
-                printf("Error: Invalid Number");
-            }
-            if(v.err==LERR_DIV_ZERO){
-                printf("Error: Division By Zero");
-            }
-        break;
+
+lval* lval_read_num(mpc_ast_t* t){
+    errno=0;
+    double x=strtod(t->contents,NULL);
+    return errno!=ERANGE?
+        lval_num(x):lval_err("invalid number");
+}
+
+lval* lval_add(lval* v,lval* x){
+    v->count++;
+    v->cell=(struct lval**)realloc(v->cell,sizeof(lval*)*v->count);
+    v->cell[v->count-1]=x;
+    return v;
+}
+
+lval* lval_read(mpc_ast_t* t){
+    if(strstr(t->tag,"number")){return lval_read_num(t);}
+    if(strstr(t->tag,"symbol")){return lval_sym(t->contents);}
+
+    lval* x=NULL;
+    //if root (>) or sexpr, create empty list
+    if(strcmp(t->tag,">")==0)   {x=lval_sexpr();}
+    if(strcmp(t->tag,"sexpr"))  {x=lval_sexpr();}
+
+    //fill the list with any valid expression ontained within
+    for(int i=0;i<t->children_num;i++){
+        char* content=t->children[i]->contents;
+        if(strcmp(content,"(")==0){continue;}
+        if(strcmp(content,")")==0){continue;}
+        if(strcmp(content,"{")==0){continue;}
+        if(strcmp(content,"}")==0){continue;}
+        if(strcmp(t->children[i]->tag,"regex")==0){continue;}
+        x=lval_add(x,lval_read(t->children[i]));
+    }
+
+    return x;
+}
+
+
+void lval_print(lval* v){
+    switch(v->type){
+        case LVAL_NUM:printf("%f",v->num);break;
+        case LVAL_ERR:printf("Error :%s",v->err);break;
+        case LVAL_SYM:printf("%s",v->sym);break;
+        case LVAL_SEXPR:lval_expr_print(v,'(',')');break;
     }
 }
 
-void lval_println(lval v){
+void lval_expr_print(lval* v,char open,char close){
+    putchar(open);
+    for(int i=0;i<v->count;i++){
+        lval_print(v->cell[i]);
+        if(i!=(v->count-1)){
+            putchar(' ');
+        }
+    }
+    putchar(close);
+}
+
+
+
+void lval_println(lval* v){
     lval_print(v);
     putchar('\n');
 }
 
+/*
 lval eval_op(lval x,char* op,lval y){
     if(strcmp(op,"+")==0){return lval_num(x.num+y.num);}
     if(strcmp(op,"-")==0){return lval_num(x.num-y.num);}
@@ -145,7 +194,7 @@ lval eval(mpc_ast_t* t){
     }
     return ret;
 }
-
+*/
 
 int number_of_nodes(mpc_ast_t* t){
     if(t->children_num == 0)
@@ -184,16 +233,16 @@ int main(int argc,char* argv[])
     puts("Lispy Version 0.0.0.0.4");
     puts("press Ctrl+c to Exit\n");
     while(1){
-        char* input=readline("clisp> ");        
+        char* input=readline("clisp> ");
         add_history(input);
         //解析器
         mpc_result_t r;
         if(mpc_parse("<stdin>",input,Lispy,&r)){
-            
-            lval result=eval(r.output);
-            lval_println(result);
+
+            lval*x =lval_read(r.output);
+            lval_println(x);
             //mpc_ast_print(r.output);
-            mpc_ast_delete(r.output);
+            lval_del(x);
         }else{
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
